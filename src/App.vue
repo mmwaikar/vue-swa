5a8cf5496232b34b469c8b8b425456832d1a9d62
Manoj <mmwaikar@gmail.com>
2023-04-24 15:03:29 +0530

    Add a HelloWorld component.
    - use it in the App component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
-  <div>Hello {{ value }}</div>
+  <HelloWorld />
+
+  <p></p>
+  <div><a v-bind:href="swaUrl">Azure static web apps</a> with <a v-bind:href="vueUrl">Vue.js</a> are amazing!</div>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue';
+
 export default {
-  name: "App",
-  data() {
-    return {
-      value: "World"
-    };
-  }
+    name: "App",
+    data() {
+        return {
+            swaUrl: "https://azure.microsoft.com/en-in/products/app-service/static/",
+            vueUrl: "https://vuejs.org/"
+        };
+    },
+    components: { HelloWorld }
 };
 </script>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>Hello {{ msg }}</div>
+</template>
+  
+<script>
+export default {
+    name: "HelloWorld",
+    data() {
+        return {
+            msg: "World"
+        };
+    }
+};
+</script>
+  
